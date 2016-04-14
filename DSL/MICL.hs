634febb9416e137f7b5b21a146c04aff5d400d7c
abbottk <abbottk@eecs.oregonstate.edu>
2016-04-14 09:14:55 -0700

    Combined Lateral, Longitudinal, and Vertical movements for better pattern matching.

--- DSL/MICL.hs
+++ DSL/MICL.hs
@@ -10,21 +10,18 @@
 module MICL where
 
 import Data.Int
-import System.IO
 
 type Domain = Signal -> State
 
 type State = (Servos,Display,Status)
 
 
--- | floating point values are most commonly given in 0.25 increments to
---       indicate the amount of power being sent to each of the engines.
+-- | floating point values indicate the amount of voltage (power) being
+--       transferred to the servos spinning the props.
 --   sequence: is an integer value representing the command's location in the
 --       sequence of all other commands
 --   channel: interprets how the drone receives commands (either from the user
---       or the computer):
---           * a value of false indicates a human agent
---           * a value of true indicates a computer agent
+--       or the computer agent)
 --   flag: interprets how the drone interprets the progressive commands from the
 --       user or the program:
 --           * bit 0: enable/disable progressive commands
@@ -51,50 +48,53 @@ type State = (Servos,Display,Status)
 --           * a negative value will make the drone spin (turn) left
 --           * a positive value will make the drone spin (turn) right
 --
-data Signal = Signal { index :: Int,
-                       channel :: Bool,
+data Signal = Signal { sequence :: Int,
+                       channel :: Agent,
                        flag :: Int32,
                        roll :: Float,
                        pitch :: Float,
                        gaz :: Float,
                        yaw :: Float }
+            deriving(Show)
 
 type Servos = (Float,Float,Float,Float)
 
-type Display = IO ()
+type Display = String
 
 type Status = (Agent,Mode)
 
 data Agent = Human
            | Computer
+           deriving(Show)
 
 data Mode = Recovery
           | Normal
           | Return
           | Wait
+          deriving(Show)
 
 
--- | servo functions that take a signal and apply the appropriate servo
---       changes (the range of input from the signal is -1.0 to 1.0 in
---       0.25 increments representing 1/4 powers of engine speed).
+-- | movement functions
+--   take a signal and apply the appropriate servo changes (the range of input
+--       from the signal is -1.0 to 1.0).
 --
-moveLeft :: Signal -> Servos -> Servos
-moveLeft sig srv = mapLeftSignal (flip (-) (get_r sig)) srv
+moveLat :: Signal -> Servos -> Servos
+moveLat sig srv
+  | (roll sig) < 0 = mapLeftSignal (flip (-) (roll sig)) srv
+  | (roll sig) > 0 = mapRghtSignal (+ (roll sig)) srv
+  | otherwise      = srv
 
-moveRght :: Signal -> Servos -> Servos
-moveRght sig srv = mapRghtSignal (+ (get_r sig)) srv
+moveLong :: Signal -> Servos -> Servos
+moveLong sig srv
+  | (pitch sig) < 0 = mapFrntSignal (flip (-) (pitch sig)) srv
+  | (pitch sig) > 0 = mapBackSignal (+ (pitch sig)) srv
+  | otherwise     = srv
 
-moveFwd :: Signal -> Servos -> Servos
-moveFwd sig srv = mapFrntSignal (flip (-) (get_p sig)) srv
-
-moveBack :: Signal -> Servos -> Servos
-moveBack sig srv = mapBackSignal (+ (get_p sig)) srv
-
-moveUp :: Signal -> Servos -> Servos
-moveUp sig srv = mapSignal (+ (get_g sig)) srv
-
-moveDown :: Signal -> Servos -> Servos
-moveDown sig srv = mapSignal (flip (-) (get_g sig)) srv
+moveElev :: Signal -> Servos -> Servos
+moveElev sig srv
+  | (gaz sig) < 0 = mapSignal (flip (-) (gaz sig)) srv
+  | (gaz sig) > 0 = mapSignal (+ (gaz sig)) srv
+  | otherwise     = srv
 
 
 -- | status functions
@@ -104,16 +104,16 @@ moveDown sig srv = mapSignal (flip (-) (get_g sig)) srv
 --       drone
 --
 switchAgent :: Signal -> Agent
-switchAgent sig = case (get_c sig) of
-  True -> Computer
-  _    -> Human
+switchAgent sig = case (channel sig) of
+  Computer -> Human
+  Human    -> Computer
 
 switchMode :: Status -> Mode
 switchMode sts = case (snd sts) of
   Recovery -> Normal
-  Normal   -> Wait
   Return   -> Normal
   Wait     -> Recovery
+  _        -> (snd sts)
 
 
 -- | combinator functions
@@ -121,20 +121,22 @@ switchMode sts = case (snd sts) of
 --
 status :: Signal -> Status -> Status
 status sig sts = case (snd sts) of
-  Return -> case (get_c sig) of
-    True -> (Human   ,switchMode sts)
-    _    -> (Computer,switchMode sts)
-  _      -> (switchAgent sig,switchMode sts)
+  Recovery -> (fst sts,switchMode sts)
+  Return -> case (channel sig) of
+    Computer -> (switchAgent sig,switchMode sts)
+    _        -> (switchAgent sig,switchMode sts)
+  Wait     -> (fst sts,switchMode sts)
+  _        -> sts
 
 movement :: Signal -> Servos -> Servos
 movement sig srv
-  | (get_r sig) < 0 = moveLeft sig srv
-  | (get_r sig) > 0 = moveRght sig srv
-  | (get_p sig) < 0 = moveFwd  sig srv
-  | (get_p sig) > 0 = moveBack sig srv
-  | (get_g sig) < 0 = moveDown sig srv
-  | (get_g sig) > 0 = moveUp   sig srv
-  | otherwise       = srv
+  | (roll sig)  /= 0 = moveLat  sig srv
+  | (pitch sig) /= 0 = moveLong sig srv
+  | (gaz sig)   /= 0 = moveElev sig srv
+
+movements :: [Signal] -> Servos -> Servos
+movements []     srv = srv
+movements (s:ss) srv = movement s (movements ss srv)
 
 task :: Signal -> Display -> Display
 task = undefined
@@ -143,46 +145,31 @@ interaction :: Signal -> Servos -> Display -> Status -> (Servos,Display,Status)
 interaction sig srv dis sts = (movement sig srv,task sig dis,status sig sts)
 
 
--- | record accessing helper functions for the signals
---
-get_i :: Signal -> Int
-get_i = index
-
-get_c :: Signal -> Bool
-get_c = channel
-
-get_f :: Signal -> Int32
-get_f = flag
-
-get_r :: Signal -> Float
-get_r = roll
-
-get_p :: Signal -> Float
-get_p = pitch
-
-get_g :: Signal -> Float
-get_g = gaz
-
-get_y :: Signal -> Float
-get_y = yaw
-
-
--- | mapping function to apply changes to all servos based on a signal
+-- | mapping function to apply changes to the correct servos based on a signal.
+--   mapSignal: increases voltage (power) to all servos
+--   mapFrntSignal: increases voltage (power) to the left rear and right rear
+--       servos
+--   mapBackSignal: increases voltage (power) to the left front and right front
+--       servos
+--   mapLeftSignal: increases voltage (power) to the right front and right rear
+--       servos
+--   mapRghtSignal: increases voltage (power) to the left front and left rear
+--       servos
 --
 mapSignal :: (a -> b) -> (a,a,a,a) -> (b,b,b,b)
-mapSignal f (a1,a2,a3,a4) = (f a1,f a2,f a3,f a4)
+mapSignal f (lf,rf,lr,rr) = (f lf,f rf,f lr,f rr)
 
-mapFrntSignal :: (a -> b) -> (a,a,a,a) -> (b,b,a,a)
-mapFrntSignal f (a1,a2,a3,a4) = (f a1,f a2,a3,a4)
+mapFrntSignal :: (a -> b) -> (a,a,a,a) -> (a,a,b,b)
+mapFrntSignal f (lf,rf,lr,rr) = (lf,rf,f lr,f rr)
 
-mapBackSignal :: (a -> b) -> (a,a,a,a) -> (a,a,b,b)
-mapBackSignal f (a1,a2,a3,a4) = (a1,a2,f a3,f a4)
+mapBackSignal :: (a -> b) -> (a,a,a,a) -> (b,b,a,a)
+mapBackSignal f (lf,rf,lr,rr) = (f lf,f rf,lr,rr)
 
-mapLeftSignal :: (a -> b) -> (a,a,a,a) -> (b,a,b,a)
-mapLeftSignal f (a1,a2,a3,a4) = (f a1,a2,f a3,a4)
+mapLeftSignal :: (a -> b) -> (a,a,a,a) -> (a,b,a,b)
+mapLeftSignal f (lf,rf,lr,rr) = (lf,f rf,lr,f rr)
 
-mapRghtSignal :: (a -> b) -> (a,a,a,a) -> (a,b,a,b)
-mapRghtSignal f (a1,a2,a3,a4) = (a1,f a2,a3,f a4)
+mapRghtSignal :: (a -> b) -> (a,a,a,a) -> (b,a,b,a)
+mapRghtSignal f (lf,rf,lr,rr) = (f lf,rf,f lr,rr)
 
 
 -- | accessing individual servos
